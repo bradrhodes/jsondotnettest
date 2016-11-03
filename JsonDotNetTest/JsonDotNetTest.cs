9a7e25b51c2f48ae47bf4574a36590c6c5a4e12a
Brad Rhodes <bradr@iqmetrix.com>
2016-11-03 15:34:48 -0600

    Created a test for deserializing to immutable classes.

---
+++ JsonDotNetTest/JsonDotNetTest.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using Ploeh.AutoFixture;
+using Xunit;
+
+namespace JsonDotNetTest
+{
+	public class JsonDotNetTest
+	{
+		[Fact]
+		public void DeserializtionTest()
+		{
+			var fixture = new Fixture();
+
+			var initial = fixture.Create<MutableClass>();
+
+			var serialized = JsonConvert.SerializeObject(initial);
+
+			var oldImmutable = JsonConvert.DeserializeObject<OldStyleImmutableClass>(serialized);
+			var newImmutable = JsonConvert.DeserializeObject<NewStyleImmutableClass>(serialized);
+			var nonFullImmutable = JsonConvert.DeserializeObject<MissingConstructorProperty>(serialized);
+
+			
+			Assert.Equal(initial.Property1, oldImmutable.Property1);
+			Assert.Equal(initial.Guid1, oldImmutable.Guid1);
+			Assert.Equal(initial.Int1, oldImmutable.Int1);
+
+			Assert.Equal(initial.Property1, newImmutable.Property1);
+			Assert.Equal(initial.Guid1, newImmutable.Guid1);
+			Assert.Equal(initial.Int1, newImmutable.Int1);
+
+			Assert.Equal(initial.Property1, nonFullImmutable.Property1);
+			Assert.NotEqual(initial.Guid1, nonFullImmutable.Guid1);
+			Assert.Equal(initial.Int1, nonFullImmutable.Int1);
+		}
+	}
+}
---
+++ JsonDotNetTest/MutableClass.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonDotNetTest
+{
+    public class MutableClass
+    {
+	    public string Property1 { get; set; }
+	    public Guid Guid1 { get; set; }
+
+	    public int Int1 { get; set; }
+    }
+
+	public class MissingConstructorProperty
+	{
+	    private readonly string _property1;
+	    private readonly Guid _guid1;
+	    private readonly int _int1;
+
+	    public MissingConstructorProperty(string property1, int int1)
+	    {
+		    _property1 = property1;
+		    _int1 = int1;
+	    }
+
+	    public string Property1 { get { return _property1; }  }
+	    public Guid Guid1 { get { return _guid1; } }
+
+	    public int Int1 { get { return _int1; } }
+	}
+
+    public class OldStyleImmutableClass
+    {
+	    private readonly string _property1;
+	    private readonly Guid _guid1;
+	    private readonly int _int1;
+
+	    public OldStyleImmutableClass(string property1, Guid guid1, int int1)
+	    {
+		    _property1 = property1;
+		    _guid1 = guid1;
+		    _int1 = int1;
+	    }
+
+	    public string Property1 { get { return _property1; }  }
+	    public Guid Guid1 { get { return _guid1; } }
+
+	    public int Int1 { get { return _int1; } }
+    }
+
+    public class NewStyleImmutableClass
+    {
+	    public NewStyleImmutableClass(string property1, Guid guid1, int int1)
+	    {
+		    Property1 = property1;
+		    Guid1 = guid1;
+		    Int1 = int1;
+	    }
+
+	    public string Property1 { get; }
+	    public Guid Guid1 { get; }
+
+	    public int Int1 { get; }
+    }
+}
